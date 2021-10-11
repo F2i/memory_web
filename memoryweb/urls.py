8f6b48bb61bb026593de93fba456b77b25278e2a
F2i <43247710+F2i@users.noreply.github.com>
2021-10-12 03:58:39 +0700

    deploy monitoring testing

--- memoryweb/urls.py
+++ memoryweb/urls.py
@@ -13,7 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-# import debug_toolbar
+# import debug_toolbar write some comment to push
 from django.contrib import admin
 from django.urls import path, include
 from memoryhub import views
