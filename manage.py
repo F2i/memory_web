3365f018fa56f8dc00fa491e5991bf6908fb5bed
nguyenlee97 <weggb.misaya@gmail.com>
2022-06-11 11:19:27 +0700

    bu cac

--- manage.py
+++ manage.py
@@ -13,10 +13,9 @@ def main():
         raise ImportError(
             "Couldn't import Django. Are you sure it's installed and "
             "available on your PYTHONPATH environment variable? Did you "
-            "forget to activate a virtual environment?"
-        ) from exc
+            "fuck")
     execute_from_command_line(sys.argv)
 
 
-if __name__ == '__main__':
+
     main()
